12e13cae2ced5dc8dd01a298a1eb393ead07f6bc
azuliate <harshbajwaw@gmail.com>
2024-06-03 16:32:04 +1200

    completed user verification
    Looks for the verified message from the webhooks. Adds verified role

---
+++ events/messageCreate/verifyUser.js
@@ -0,0 +1,19 @@
+module.exports = (message) => {
+  if (!message.content.toLowerCase().includes("verify")) return;
+  if (message.channel.id !== "1246221544838008853") return;
+
+  try {
+    const webhookArray = message.content.split(" ");
+    const webhookArrayThird = webhookArray[2];
+    const verifiedUserId = webhookArrayThird.replace(/[^0-9\.]+/g, "");
+
+    const member = message.guild.members;
+
+    member.addRole({
+      user: verifiedUserId,
+      role: "1247018027963121694",
+    });
+  } catch (error) {
+    console.log(`Error setting user to verified: ${error}`);
+  }
+};
