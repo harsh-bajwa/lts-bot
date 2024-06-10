38c86ada898e100e2ab36a7586f581f36c2ecb3c
azuliate <harshbajwaw@gmail.com>
2024-06-11 01:51:33 +1200

    welcome embed command
    displays a welcome embed

---
+++ commands/QOL/welcome-embed.js
@@ -0,0 +1,34 @@
+const { EmbedBuilder } = require("discord.js");
+const ltsImage = require("../../image/LTS");
+
+module.exports = {
+  data: {
+    name: "welcome-embed",
+    description: "This is for the welcome channel",
+  },
+
+  run: async (interaction) => {
+    const embed = new EmbedBuilder()
+      .setColor("DarkButNotBlack")
+      .setTitle("Leverage through Systems")
+      .setURL("https://systemising.com")
+      .setAuthor({
+        name: "azuliate",
+        iconURL: "https://pbs.twimg.com/media/DmTd3qCX0AAJprP.jpg",
+      })
+      .setDescription(
+        "Welcome to Leverage through Systems. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      )
+      .addFields({
+        name: "verify",
+        value: "use /verify in <#1246221544838008853> to unlock content.",
+        inline: true,
+      })
+      .setImage(
+        "https://cdn.discordapp.com/attachments/1244461096186675251/1247852219693531166/Untitled_design_5.png?ex=66681ff1&is=6666ce71&hm=dcf9f847d0b8c96af4237706e5d0be9949231bf3c88191f9f7ea2421f3d51664&"
+      )
+      .setTimestamp();
+
+    await interaction.interaction.reply({ embeds: [embed] });
+  },
+};
